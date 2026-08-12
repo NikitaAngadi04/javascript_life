a4f6d03a59a43b1dfdd69069335edc3efbcf53a4
Nikita Angadi <nikitarap1c014@gmail.com>
2026-08-12 13:13:17 +0530

    Task

--- Functions/Functions.js
+++ Functions/Functions.js
@@ -1,66 +1,84 @@
-//Functions in Js
+// Functions in Js
 
-// function print(){
-//     console.log("Hello Iam Function")
-// }
-// print()
-
-
-//*arguments and parameteres
-// function print(Name){
-//     console.log("Hello Iam", Name)
-// }
-// print("Nikita")
-
-//named function
-// function print(Name){
-//     console.log("Hello Iam", Name)
-// }
-// print("Nikita")
-
-//Return keyword
-// function add(a,b){
-//     return a+b
-// }
-// let res = add(10,20)
-// console.log(res)
+function print(){
+    console.log("Hello Iam Function")
+}
+print()
 
 
-// function add(a,b){
-//     return a+b
-//     gbfhjeghukjvn           it will not return this value afetr return you can write anything, and also it will not give error if you define before it will throw error
-// }
-// let res = add(10,20)
-// console.log(res)
+// *arguments and parameteres
+function print(Name){
+    console.log("Hello Iam", Name)
+}
+print("Nikita")
 
-//anonymous function doesnot have any any name , and we are storing into variable and we able to call function
-// let print=function(){
-//     console.log("Hello Iam anonymous function");
-// }
-// print()
+// named function
+function print(Name){
+    console.log("Hello Iam", Name)
+}
+print("Nikita")
+
+// Return keyword
+function add(a,b){
+    return a+b
+}
+let res = add(10,20)
+console.log(res)
 
 
-//arrow function (fat arrow function)
-//single line of code no need of curly braces
-//sinle line line of code no need of parenthesis
-//parameter handle
-// let adding = (a)=>{
-//     return a+100
-// }
-// console.log(adding(10))
+function add(a,b){
+    return a+b
+    // gbfhjeghukjvn           it will not return this value afetr return you can write anything, and also it will not give error if you define before it will throw error
+}
+let res = add(10,20)
+console.log(res)
 
-//advantage
-// let adding=_=>10+100
-// console.log(adding(10))
+// anonymous function doesnot have any any name , and we are storing into variable and we able to call function
+let print=function(){
+    console.log("Hello Iam anonymous function");
+}
+print()
 
-// let add = (a, b) => a+b
-// console.log (add (10,20))
 
-//find a given number is even and odd
-//using arrow function find greatest of 3
-//using arrow function get a factorial of number
-//
+// arrow function (fat arrow function)
 
+let adding = (a)=>{
+    return a+100
+}
+console.log(adding(10))
+
+// advantage
+let adding=_=>10+100
+console.log(adding(10))
+
+let add = (a, b) => a+b
+console.log (add (10,20))
+
+//find a greatest of 6 numbers
+
+let a = 10;
+let b = 25;
+let c = 15;
+let d = 40;
+let e = 30;
+let f = 20;
+
+let greatest =
+    (a > b && a > c && a > d && a > e && a > f) ? a :
+    (b > c && b > d && b > e && b > f) ? b :
+    (c > d && c > e && c > f) ? c :
+    (d > e && d > f) ? d :
+    (e > f) ? e : f;
+
+console.log("Greatest number is:", greatest);
+
+//find a squre of number using arrow function
+let square = (num) => num * num;
+
+console.log(square(5));
+console.log(square(12));
+
+//find a given number is even and odd 
 let even =(x)=>{
      if(x%2==0){
         console.log("even")
@@ -71,7 +89,7 @@ let even =(x)=>{
 let res=even(6)
 console.log(res) 
 
-
+//using arrow function find greatest of 3
 let a = 90
 let b = 5
 let c =8
